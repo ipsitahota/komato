a782136071fadfcb8d16d3dba2d04a21fcc1ff7e
ipsita hota <ipsitahota48@gmail.com>
2023-12-22 00:44:35 +0530

    second day completed

--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_kitchen/utils/colors.dart';
+import 'package:flutter_kitchen/widgets/big_text.dart';
+import 'package:flutter_kitchen/widgets/small_text.dart';
 
 class MainFoodPage extends StatefulWidget {
   const MainFoodPage({super.key});
@@ -15,25 +18,33 @@ class _MainFoodPageState extends State<MainFoodPage> {
         children: [
           Container(
       child: Container(
-        margin: EdgeInsets.only(top: 45,bottom: 15),
-        padding: EdgeInsets.only(left: 20,right: 20),
+        margin: const EdgeInsets.only(top: 45,bottom: 15),
+        padding: const EdgeInsets.only(left: 20,right: 20),
         child:Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
           Column(
               children: [
-                Text("country"),
-                Text("city")
+               BigText(text: "India",color: AppColors.mainColor ,),
+              Center(
+                child: Row(
+                  children: [
+                SmallText(text: "Mumbai",color: Colors.black54,),
+                const Icon(Icons.arrow_drop_down_rounded)
+                  ],
+                ),
+              )
               ],
             ),
             Center(
               child: Container(
                 width: 45,
                 height: 45,
-                child: Icon(Icons.search,color: Colors.white),
+                // ignore: sort_child_properties_last
+                child: const Icon(Icons.search,color: Colors.white),
                  decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(15),
-                  color: Colors.cyanAccent
+                  color: AppColors.mainColor,
                  ),
               ),
             )
